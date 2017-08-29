8e7301e798c4e1227ae13d8525c89a187728efb2
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 15:47:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,5 +33,8 @@ function prependKitten(name) {
 function removeLastKitten() {
   var newArray = kittens.slice(0, kittens.length - 1)
   return newArray
+}
 
+function removeFirstKitten() {
+  var newArray = kittens.slice(1, kittens.length)
 }
