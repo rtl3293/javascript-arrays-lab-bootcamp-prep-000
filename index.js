f496b829a8b7dbbaa9b78a09237acf2fde60b1c3
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-08-29 15:22:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = "I don't do much."
-kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"]
+
 function destructivelyAppendKitten(name) {
   kittens.shift(name)
   return kittens
